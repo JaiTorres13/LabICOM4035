40d45f4c87b02a278a6c085edd5afde3a5b0ad48
JaiTorres13 <jainel.torres@upr.edu>
2018-03-26 21:58:05 -0400

    finished implementing lab5 part 2

--- Lab5 Part2/menuClasses/DeleteFromListAction.java
+++ Lab5 Part2/menuClasses/DeleteFromListAction.java
@@ -1,10 +1,17 @@
 package menuClasses;
 
+import dataManager.DMComponent;
+import ioManagementClasses.IOComponent;
+
 public class DeleteFromListAction implements Action {
 
 	@Override
 	public void execute(Object args) {
-		// TODO Auto-generated method stub
+		IOComponent io = IOComponent.getComponent(); 
+		DMComponent dm = (DMComponent) args; 
+		String name = io.getInput("Enter the name of the list to remove: ");
+		int value = Integer.parseInt(io.getInput("Enter a value to remove: ")); 
+		dm.removeElementFromList(name, value); 	
 
 	}
 
