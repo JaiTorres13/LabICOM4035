b9b90b7b419a7441df86bae19dba4b604acc4bc8
JaiTorres13 <jainel.torres@upr.edu>
2018-05-07 00:46:32 -0400

    deleted lab 11

--- Lab11(Sorting)/testerClasses/DataReader.java
+++
@@ -1,9 +0,0 @@
-package testerClasses;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-
-public interface DataReader<E> {
-	ArrayList<E> readDataFromFile() throws FileNotFoundException; 
-}
